4a06c59ca6d6504fd323bc95ecb119e7ab975b8b
AyushKoul124 <147028666+AyushKoul124@users.noreply.github.com>
2023-12-01 00:35:29 +0530

    Create STRLEN.C

---
+++ STRLEN.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+main()
+{
+int len;
+char job[30];
+clrscr();
+printf("\n\n\n\tEnter Your Job : ");
+gets(job);
+len=strlen(job);
+printf("\tYour
